31d258ba9ee2853f359b658520012a2578d22c3b
ktranfaglia1 <kyletranfaglia@gmail.com>
2025-02-19 10:41:36 -0500

    Merge datasets

---
+++ MergeData.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+df1 = pd.read_csv("./Data/Survey_data.csv")
+df2 = pd.read_csv("./Data/Standerized_SP_data.csv")
+
+print (df1.shape)
+
+df1["Q2"] = df2["Q2"]
+df1["Q3"] = df2["Q3 (Minutes)"]
+df1["Q4"] = df2["Q4"]
+df1["Q5"] = df2["Q5 (Hours)"]
+
+print (df1["Q2"])
+print (df1["Q3"])
+print (df1["Q4"])
+print (df1["Q5"])
+
+print (df1.shape)
+
+df1.to_csv("./Data/Survey_data.csv")
