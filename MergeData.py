c5d33ba9766c861e04f0f257db7d0a2a7e310fb4
ktranfaglia1 <kyletranfaglia@gmail.com>
2025-02-19 17:28:15 -0500

    Readded attention score column

--- MergeData.py
+++ MergeData.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 # Read in data to merge
 df1 = pd.read_csv("./Data/Survey_data.csv")
-df2 = pd.read_csv("./Data/Standerized_SP_data.csv")
+df2 = pd.read_csv("./Data/Survey_data_test.csv")
 
 print (df1.shape)
 
@@ -17,7 +17,12 @@ print (df1["Q3"])
 print (df1["Q4"])
 print (df1["Q5"])
 
+df1['Attention_score'] = df2['attention_pass_count']
+
 print (df1.shape)
+
+# Keep columns that meet the criteria
+df1 = df1[[col for col in df1.columns if col.startswith("Q")] + ["Duration (in seconds)"] + ["Attention_score"]]
 
 # Export the dataset
 df1.to_csv("./Data/Survey_data.csv")
